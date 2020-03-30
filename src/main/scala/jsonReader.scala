aafe5cf64de5a7a0c1a8c26c84cd2206c3cb9501
shymanovich <shymanovichvlad@gmail.com>
2020-03-30 03:49:18 +0300

    init

---
+++ src/main/scala/jsonReader.scala
@@ -0,0 +1,25 @@
+import org.apache.spark.{SparkConf, SparkContext}
+import org.json4s._
+import org.json4s.jackson.JsonMethods._
+
+case class wineryProduction(
+                 country: String,
+                 id: Int,
+                 points: Int,
+                 price: Int,
+                 title: String,
+                 variety: String,
+                 winery: String)
+
+object jsonReader {
+  val conf = new SparkConf().setAppName("JsonReader")
+  conf.setMaster("local");
+  val sc = new SparkContext(conf)
+
+  def main(args: Array[String]): Unit = {
+    val text = sc.textFile(args(0))
+
+    text.foreach(x => {implicit val formats = DefaultFormats; println(parse(x).extractOpt[wineryProduction])})
+
+  }
+}
